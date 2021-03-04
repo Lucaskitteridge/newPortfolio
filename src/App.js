cbf88f0dc8eb52d73ac2cccb43edad07a7f95ff7
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-04 13:20:51 -0500

    adding navbar and welcome page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Nav, Jumbotron } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="Homebeforescrolling">
+      <Navbar className="Navbar" fixed="top">
+        <div>
+          <Navbar.Brand href="#home">Lucas Kitteridge</Navbar.Brand>
+        </div>
+        <div>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#features">Projects</Nav.Link>
+            <Nav.Link href="#home">Contact</Nav.Link>
+          </Nav>
+        </div>
+      </Navbar>
+      <div className="welcomepage">
+        <h1>Hello, world!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      </div>
     </div>
   );
 }
