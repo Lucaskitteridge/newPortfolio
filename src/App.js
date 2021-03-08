35e75b6dd2c2f398410c7966769037a578e034d4
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-08 16:09:05 -0500

    hover added

--- src/App.js
+++ src/App.js
@@ -6,30 +6,28 @@ import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 function App() {
 
-
   return (
     <div className="App">
-      <div className="Homebeforescrolling">
-        <Navbar className="Navbar" sticky="top" id="navbar">
-          <div>
-            <Navbar.Brand href="#home">Lucas Kitteridge</Navbar.Brand>
-          </div>
-          <div>
-            <Nav className="mr-auto">
-              <Nav.Link href="#aboutMe">About</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-          </div>
-        </Navbar>
-        <div className="welcomepage">
-          <h1>Hi, I'm Lucas and I'm a full stack web-developer</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-          </p>
+      <Navbar className="Navbar" sticky="top" id="navbar">
+        <div>
+          <Navbar.Brand href="#home">Lucas Kitteridge</Navbar.Brand>
         </div>
+        <div>
+          <Nav className="mr-auto">
+            <Nav.Link href="#aboutMe">About</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </div>
+      </Navbar>
+      <div className="welcomepage">
+        <h1>Hi, I'm Lucas and I'm a full stack web-developer</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+          </p>
       </div>
+
       <div id="aboutMe" className="aboutme">
         <h1>About Me</h1>
         <p>
@@ -38,7 +36,7 @@ function App() {
           </p>
       </div>
       <div id="projects" className="projects">
-        <h1>About Me</h1>
+        <h1>Projects</h1>
         <p>
           This is a simple hero unit, a simple jumbotron-style component for calling
           extra attention to featured content or information.
@@ -48,13 +46,13 @@ function App() {
         <h1>Contact</h1>
         <div className="icons">
           <a href="https://www.linkedin.com/in/lucaskitteridge/">
-            <FaLinkedin />
+            <FaLinkedin className="Fa" size="2em" />
           </a>
           <a href="https://github.com/Lucaskitteridge">
-            <FaGithub />
+            <FaGithub className="Fa" size="2em" />
           </a>
           <a href="mailto:lucas.benjamin.kitteridge@gmail.com">
-            <FaEnvelope />
+            <FaEnvelope className="Fa" size="2em" />
           </a>
         </div>
       </div>
