090f05f169edc1bf61221c435a4ff59bce11196f
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-08 16:25:35 -0500

    switched photos

--- src/App.js
+++ src/App.js
@@ -9,25 +9,26 @@ function App() {
   return (
     <div className="App">
       <Navbar className="Navbar" sticky="top" id="navbar">
-        <div>
-          <Navbar.Brand href="#home">Lucas Kitteridge</Navbar.Brand>
-        </div>
-        <div>
-          <Nav className="mr-auto">
-            <Nav.Link href="#aboutMe">About</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </div>
-      </Navbar>
-      <div className="welcomepage">
-        <h1>Hi, I'm Lucas and I'm a full stack web-developer</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
+          <div>
+            <Navbar.Brand href="#welcomepage">Lucas Kitteridge</Navbar.Brand>
+          </div>
+          <div>
+            <Nav className="mr-auto">
+              <Nav.Link href="#aboutMe">About</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </div>
+        </Navbar>
+      <div className="Homebeforescrolling">
+        <div className="welcomepage" id="welcomepage">
+          <h1>Hi, I'm Lucas and I'm a full stack web-developer</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
           </p>
+        </div>
       </div>
-
       <div id="aboutMe" className="aboutme">
         <h1>About Me</h1>
         <p>
@@ -46,13 +47,13 @@ function App() {
         <h1>Contact</h1>
         <div className="icons">
           <a href="https://www.linkedin.com/in/lucaskitteridge/">
-            <FaLinkedin className="Fa" size="2em" />
+            <FaLinkedin className="Fa"size="2em" />
           </a>
           <a href="https://github.com/Lucaskitteridge">
-            <FaGithub className="Fa" size="2em" />
+            <FaGithub className="Fa"size="2em" />
           </a>
           <a href="mailto:lucas.benjamin.kitteridge@gmail.com">
-            <FaEnvelope className="Fa" size="2em" />
+            <FaEnvelope className="Fa"size="2em"/>
           </a>
         </div>
       </div>
