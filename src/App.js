c391c85aca22f26cce2bf628ad4f7f7bfbf7e504
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-08 16:38:14 -0500

    icon caption added

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar className="Navbar" sticky="top" id="navbar">
-          <div>
-            <Navbar.Brand href="#welcomepage">Lucas Kitteridge</Navbar.Brand>
-          </div>
-          <div>
-            <Nav className="mr-auto">
-              <Nav.Link href="#aboutMe">About</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-          </div>
-        </Navbar>
+      <Navbar className="Navbar" position="relative" sticky="top" id="navbar">
+        <div>
+          <Navbar.Brand href="#welcomepage">Lucas Kitteridge</Navbar.Brand>
+        </div>
+        <div>
+          <Nav className="mr-auto">
+            <Nav.Link href="#aboutMe">About</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </div>
+      </Navbar>
       <div className="Homebeforescrolling">
         <div className="welcomepage" id="welcomepage">
           <h1>Hi, I'm Lucas and I'm a full stack web-developer</h1>
@@ -46,15 +46,24 @@ function App() {
       <div id="contact" className="contact">
         <h1>Contact</h1>
         <div className="icons">
-          <a href="https://www.linkedin.com/in/lucaskitteridge/">
-            <FaLinkedin className="Fa"size="2em" />
-          </a>
-          <a href="https://github.com/Lucaskitteridge">
-            <FaGithub className="Fa"size="2em" />
-          </a>
-          <a href="mailto:lucas.benjamin.kitteridge@gmail.com">
-            <FaEnvelope className="Fa"size="2em"/>
-          </a>
+          <div>
+            <a href="https://www.linkedin.com/in/lucaskitteridge/">
+              <FaLinkedin className="Fa" size="2em" />
+            </a>
+            <p className="linkname">linkedin</p>
+          </div>
+          <div>
+            <a href="https://github.com/Lucaskitteridge">
+              <FaGithub className="Fa" size="2em" />
+            </a>
+            <p className="linkname">Github</p>
+          </div>
+          <div>
+            <a href="mailto:lucas.benjamin.kitteridge@gmail.com">
+              <FaEnvelope className="Fa" size="2em" />
+            </a>
+            <p className="linkname">Email me</p>
+          </div>
         </div>
       </div>
     </div>
