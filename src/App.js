dac1caf84f2dc225ddcc75347bea78845f2c9f8c
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-08 17:26:51 -0500

    scrolling top image

--- src/App.js
+++ src/App.js
@@ -9,64 +9,67 @@ function App() {
   return (
     <div className="App">
       <div className="Homebeforescrolling">
-      <Navbar className="Navbar" position="fixed" sticky="top" id="navbar">
-        <div>
-          <Navbar.Brand href="#welcomepage">Lucas Kitteridge</Navbar.Brand>
+        <Navbar className="Navbar" position="fixed" sticky="top" id="navbar">
+          <div>
+            <Navbar.Brand href="#welcomepage">Lucas Kitteridge</Navbar.Brand>
+          </div>
+          <div>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto" >
+                <Nav.Link href="#aboutMe">About</Nav.Link>
+                <Nav.Link href="#projects">Projects</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Navbar>
+        <div >
+          <div className="welcomepage" id="welcomepage">
+            <h1>Hi, I'm Lucas and I'm a full stack web-developer</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+          </p>
+          </div>
         </div>
-        <div>
-          <Nav className="mr-auto">
-            <Nav.Link href="#aboutMe">About</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </div>
-      </Navbar>
-      <div >
-        <div className="welcomepage" id="welcomepage">
-          <h1>Hi, I'm Lucas and I'm a full stack web-developer</h1>
+        <div id="aboutMe" className="aboutme">
+          <h1>About Me</h1>
           <p>
             This is a simple hero unit, a simple jumbotron-style component for calling
             extra attention to featured content or information.
           </p>
         </div>
-      </div>
-      <div id="aboutMe" className="aboutme">
-        <h1>About Me</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
+        <div id="projects" className="projects">
+          <h1>Projects</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
           </p>
-      </div>
-      <div id="projects" className="projects">
-        <h1>Projects</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
-          </p>
-      </div>
-      <div id="contact" className="contact">
-        <h1>Contact</h1>
-        <div className="icons">
-          <div>
-            <a href="https://www.linkedin.com/in/lucaskitteridge/">
-              <FaLinkedin className="Fa" size="2em" />
-            </a>
-            <p className="linkname">linkedin</p>
-          </div>
-          <div>
-            <a href="https://github.com/Lucaskitteridge">
-              <FaGithub className="Fa" size="2em" />
-            </a>
-            <p className="linkname">Github</p>
-          </div>
-          <div>
-            <a href="mailto:lucas.benjamin.kitteridge@gmail.com">
-              <FaEnvelope className="Fa" size="2em" />
-            </a>
-            <p className="linkname">Email me</p>
+        </div>
+        <div id="contact" className="contact">
+          <h1>Contact</h1>
+          <div className="icons">
+            <div>
+              <a href="https://www.linkedin.com/in/lucaskitteridge/">
+                <FaLinkedin className="Fa" size="2em" />
+              </a>
+              <p className="linkname">linkedin</p>
+            </div>
+            <div>
+              <a href="https://github.com/Lucaskitteridge">
+                <FaGithub className="Fa" size="2em" />
+              </a>
+              <p className="linkname">Github</p>
+            </div>
+            <div>
+              <a href="mailto:lucas.benjamin.kitteridge@gmail.com">
+                <FaEnvelope className="Fa" size="2em" />
+              </a>
+              <p className="linkname">Email me</p>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
