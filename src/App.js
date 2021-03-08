d1f70defc08d33f93b0dc8235f2dd71a79a35676
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-08 13:11:59 -0500

    bootstrap icons added

--- src/App.js
+++ src/App.js
@@ -4,28 +4,50 @@ import { Button, Navbar, Nav, Jumbotron } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+
   return (
     <div className="App">
       <div className="Homebeforescrolling">
-      <Navbar className="Navbar" fixed="top">
-        <div>
-          <Navbar.Brand href="#home">Lucas Kitteridge</Navbar.Brand>
+        <Navbar className="Navbar" sticky="top" id="navbar">
+          <div>
+            <Navbar.Brand href="#home">Lucas Kitteridge</Navbar.Brand>
+          </div>
+          <div>
+            <Nav className="mr-auto">
+              <Nav.Link href="#aboutMe">About</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </div>
+        </Navbar>
+        <div className="welcomepage">
+          <h1>Hi, I'm Lucas and I'm a full stack web-developer</h1>
+          <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" class="bi bi-github" viewBox="0 0 16 16">
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
         </div>
-        <div>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">About</Nav.Link>
-            <Nav.Link href="#features">Projects</Nav.Link>
-            <Nav.Link href="#home">Contact</Nav.Link>
-          </Nav>
-        </div>
-      </Navbar>
-      <div className="welcomepage">
-        <h1>Hello, world!</h1>
+      </div>
+      <div id="aboutMe" className="aboutme">
+        <h1>About Me</h1>
         <p>
           This is a simple hero unit, a simple jumbotron-style component for calling
           extra attention to featured content or information.
-        </p>
+          </p>
       </div>
+      <div id="projects" className="projects">
+        <h1>About Me</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+          </p>
+      </div>
+      <div id="contact" className="contact">
+        <h1>Contact</h1>
       </div>
     </div>
   );
