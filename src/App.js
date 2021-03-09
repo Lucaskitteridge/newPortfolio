11fd0131882643d7e3811ff6899c13a7e4216621
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-09 15:41:31 -0500

    template for skills added

--- src/App.js
+++ src/App.js
@@ -35,17 +35,45 @@ function App() {
           <div className="columns">
             <div className="profile"></div>
             <p>
-              This is a simple hero unit, a simple jumbotron-style component for calling
-              extra attention to featured content or information.
+              Hi, my name's Lucas. Currently I'm transitioning from a seven year career as a professional figure skater on Team GB to a career in Full stack development.
           </p>
           </div>
         </div>
         <div id="skills" className="skills">
           <h1 ClassName="title">Skills</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-          </p>
+          <div className="categories">
+            <div className="languages">
+              <h1>Languages</h1>
+              <div className="js" alt="Javascript" title="Javascript"></div>
+              <div className="ruby" alt="Ruby" title="Ruby"></div>
+              <div className="html" alt="HTML" title="Html"></div>
+              <div className="css" alt="Css" title="Css"></div>
+            </div>
+            <div className="databases">
+              <h1>Databases</h1>
+              <div className="sql" alt="PostgreSQL" title="PostgreSQL"></div>
+            </div>
+            <div className="frameworks">
+              <h1>Frameworks/libraries</h1>
+              <div className="Node" alt="Node" title="Node"></div>
+              <div className="React" alt="React" title="React"></div>
+              <div className="Express" alt="Express" title="Express"></div>
+              <div className="Websockets" alt="Websockets" title="Websockets"></div>
+              <div className="Ajax" alt="Ajax" title="Ajax"></div>
+              <div className="Jquery" alt="Jquery" title="Jquery"></div>
+              <div className="Axios" alt="Axios" title="Axios"></div>
+              <div className="Babel" alt="Babel" title="Babel"></div>
+              <div className="EJS" alt="EJS" title="EJS"></div>
+            </div>
+            <div className="Testing">
+              <h1>Testing</h1>
+              <div className="Jest" alt="Jest" title="Jest"></div>
+              <div className="Capybara" alt="Capybara" title="Capybara"></div>
+              <div className="Cypress" alt="Cypress" title="Cypress"></div>
+              <div className="Mocha" alt="Mocha" title="Mocha"></div>
+              <div className="Chai" alt="Chai" title="Chai"></div>
+            </div>
+          </div>
         </div>
         <div id="projects" className="projects">
           <h1 ClassName="title">Projects</h1>
@@ -58,13 +86,13 @@ function App() {
           <h1 ClassName="title">Contact</h1>
           <div className="icons">
             <div>
-              <a href="https://www.linkedin.com/in/lucaskitteridge/">
+              <a href="https://www.linkedin.com/in/lucaskitteridge/" target="_blank">
                 <FaLinkedin className="Fa" size="2em" />
               </a>
               <p className="linkname">linkedin</p>
             </div>
             <div>
-              <a href="https://github.com/Lucaskitteridge">
+              <a href="https://github.com/Lucaskitteridge" target="_blank">
                 <FaGithub className="Fa" size="2em" />
               </a>
               <p className="linkname">Github</p>
